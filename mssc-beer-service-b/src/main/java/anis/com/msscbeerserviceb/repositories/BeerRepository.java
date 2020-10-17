03a344a1980f3d1f2bf2c7b00a6b39fdf376871b
Anis Medini <Anis1011@gmail.com>
2020-10-17 11:48:31 -0700

    remove some unnecessary comment

--- mssc-beer-service-b/src/main/java/anis/com/msscbeerserviceb/repositories/BeerRepository.java
+++ mssc-beer-service-b/src/main/java/anis/com/msscbeerserviceb/repositories/BeerRepository.java
@@ -1,20 +1,20 @@
 package anis.com.msscbeerserviceb.repositories;
 
-import anis.com.msscbeerserviceb.domain.Beer;
 import anis.com.brewery.model.BeerStyleEnum;
+import anis.com.msscbeerserviceb.domain.Beer;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
-    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
 
-    Page<Beer> findAllByBeerName(String beerName, PageRequest pageRequest);
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
-    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, PageRequest pageRequest);
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
 
     Beer findByUpc(String upc);
 }
